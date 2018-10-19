c833a1f40defa4054db48f5568e942f3af92630e
ThousanDIY <tomorrow56@gmail.com>
2018-10-20 02:23:29 +0900

    Add files via upload

---
+++ M5_MotorTest/M5Motor.h
@@ -0,0 +1,74 @@
+#ifndef M5Motor_h_
+#define M5Motor_h_
+
+//------------------------------------------
+// Moter Driver
+//------------------------------------------
+#define TB_A1   12  // moterA IN1
+#define TB_A2   13  // moterA IN2
+#define TB_Ap   0   // moterA PWM
+#define TB_B1   2   // moterB IN1
+#define TB_B2   16  // moterB IN2
+#define TB_Bp   15  // moterB PWM
+
+#endif
+
+/*
+//  こんな感じでパラメータを設定して
+//
+    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
+        {
+            .channel    = LEDC_CHANNEL_0,
+            .duty       = 0,
+            .gpio_num   = LED_GPIO_Red,
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .timer_sel  = LEDC_TIMER_0
+        },
+        {
+            .channel    = LEDC_CHANNEL_1,
+            .duty       = 0,
+            .gpio_num   = LED_GPIO_Green,
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .timer_sel  = LEDC_TIMER_0
+        },
+        {
+            .channel    = LEDC_CHANNEL_2,
+            .duty       = 0,
+            .gpio_num   = LED_GPIO_Blue,
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .timer_sel  = LEDC_TIMER_0
+        }
+    };
+
+
+// こんな初期化ルーチンを呼び出して、
+//
+void init_ledc(ledc_channel_config_t *ledc_channel) {
+    int ch;
+    
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_10_BIT, // resolution of PWM duty
+        .freq_hz = 5000,                      // frequency of PWM signal
+        .speed_mode = LEDC_HIGH_SPEED_MODE,   // timer mode
+        .timer_num = LEDC_TIMER_0             // timer index
+    };
+
+    ledc_timer_config(&ledc_timer);
+
+    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+        ledc_channel_config(&ledc_channel[ch]);
+    }
+
+    ledc_fade_func_install(0);
+}
+
+以上で準備OK、あとはdutyに変化後の値、BlinkDurationに変化秒数を設定すれば、勝手に変化してくれます。
+
+    for (int ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+        ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
+                ledc_channel[ch].channel, duty, BlinkDuration);
+        ledc_fade_start(ledc_channel[ch].speed_mode,
+                ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
+    }            
+
+ */
---
+++ M5_MotorTest/M5_MotorTest.ino
@@ -0,0 +1,172 @@
+#include <M5Stack.h>
+#include "M5StackUpdater.h"
+
+#include "M5Motor.h"
+
+String DisplayFileName = "";
+
+//ESP32 SPEED Control sample
+
+//LEDC channel of 16 channels (started from zero)
+#define LEDC_CHANNEL_0 0
+#define LEDC_CHANNEL_1 1
+
+//use 8 bit precission for LEDC timer
+#define LEDC_TIMER_BIT 8
+
+//use 10 kHz as a LEDC base frequency
+#define LEDC_BASE_FREQ 20000
+
+int min = 0;
+int max = 255;
+
+int duration = 255; // duration time msec
+int interval = duration / (max - min);
+
+void setup_l293d() {
+  pinMode( TB_A1, OUTPUT );
+  pinMode( TB_A2, OUTPUT );
+
+  pinMode( TB_B1, OUTPUT );
+  pinMode( TB_B2, OUTPUT );
+
+  // Set Enable pin to LEDC
+  ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+
+  ledcAttachPin(TB_Ap, LEDC_CHANNEL_0);  // Motor A
+  ledcAttachPin(TB_Bp, LEDC_CHANNEL_1);  // Motor B
+   
+  digitalWrite( TB_A1, LOW );
+  digitalWrite( TB_A2, LOW );
+
+  digitalWrite( TB_B1, LOW );
+  digitalWrite( TB_B2, LOW );
+
+  ledcWrite(0, min);
+  ledcWrite(1, min);
+}
+
+void cmd_forward() {
+  Serial.println("all forward");
+  
+  digitalWrite( TB_A1, HIGH );
+  digitalWrite( TB_A2, LOW );
+  
+  digitalWrite( TB_B1, HIGH );
+  digitalWrite( TB_B2, LOW );
+
+  for(int n = min; n <= max; n++){
+    ledcWrite(0, n);
+    ledcWrite(1, n);
+    delay(interval);
+  }
+}
+
+void cmd_back() {
+  Serial.println("all back");
+  
+  digitalWrite( TB_A1, LOW );
+  digitalWrite( TB_A2, HIGH );
+  
+  digitalWrite( TB_B1, LOW );
+  digitalWrite( TB_B2, HIGH );
+
+  for(int n = min; n <= max; n++){
+    ledcWrite(0, n);
+    ledcWrite(1, n);
+    delay(interval);
+  }
+}
+
+void cmd_stop() {
+  Serial.println("all stop");
+
+  ledcWrite(0, 0);
+  ledcWrite(1, 0);
+}
+
+void cmd_spin_turn() {
+  Serial.println("spin turn");
+  
+  digitalWrite( TB_A1, HIGH );
+  digitalWrite( TB_A2, LOW );
+  
+  digitalWrite( TB_B1, LOW );
+  digitalWrite( TB_B2, HIGH );
+
+  for(int n = min; n <= max; n++){
+    ledcWrite(0, n);
+    ledcWrite(1, n);
+    delay(interval);
+  }
+}
+
+void cmd_turn_left() {
+  Serial.println("turn left");
+  
+  digitalWrite( TB_A1, HIGH );
+  digitalWrite( TB_A2, LOW );
+  
+  digitalWrite( TB_B1, HIGH );
+  digitalWrite( TB_B2, LOW );
+
+  ledcWrite(0, 0);
+
+  for(int n = min; n <= max; n++){
+    ledcWrite(1, n);
+    delay(interval);
+  }
+}
+
+void cmd_turn_right() {
+  Serial.println("turn right");
+  
+  digitalWrite( TB_A1, HIGH );
+  digitalWrite( TB_A2, LOW );
+  
+  digitalWrite( TB_B1, HIGH );
+  digitalWrite( TB_B2, LOW );
+
+  for(int n = min; n <= max; n++){
+    ledcWrite(0, n);
+    delay(interval);
+  }
+  ledcWrite(1, 0);
+}
+
+void setup() { 
+    M5.begin();
+
+    if(digitalRead(BUTTON_A_PIN) == 0) {
+      Serial.println("Will Load menu binary");
+      updateFromFS(SD);
+      ESP.restart();
+    }
+
+//    Serial.begin(115200);
+
+    setup_l293d();
+    cmd_stop();
+
+    DisplayFileName = "/jpg/M5Logo.jpg";
+    M5.Lcd.drawJpgFile(SD, DisplayFileName.c_str(), 0, 0, 320, 240);
+
+    Serial.println( "Setup done");
+}
+
+void loop() {
+    if(M5.BtnA.wasPressed()) {
+        Serial.printf("A");
+        cmd_turn_left();
+    }
+    if(M5.BtnB.wasPressed()) {
+        Serial.printf("B");
+        cmd_back();
+    } 
+    if(M5.BtnC.wasPressed()) {
+        Serial.printf("C");
+        cmd_turn_right();
+    } 
+    M5.update();
+}
